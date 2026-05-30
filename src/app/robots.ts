417b5800dc87406ed6c5b7c58f10abfc29e0b803
Alejandro Leyva <aleleyva24@gmail.com>
2026-05-30 11:46:34 -0600

    Allow AI crawlers in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,23 +3,16 @@ import { getBaseUrl } from '@/lib/utils'
 
 export default function robots(): MetadataRoute.Robots {
   const BASE_URL = getBaseUrl()
+
   return {
     rules: [
       {
         userAgent: '*',
-        allow:     '/',
-        disallow:  ['/api/', '/_next/', '/static/'],
-      },
-      {
-        userAgent: 'GPTBot',
-        disallow:  ['/'],
-      },
-      {
-        userAgent: 'CCBot',
-        disallow:  ['/'],
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/static/'],
       },
     ],
     sitemap: `${BASE_URL}/sitemap.xml`,
-    host:    BASE_URL,
+    host: BASE_URL,
   }
 }
